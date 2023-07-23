66e600b482653189dca1093f85555a969a1c6923
Amrit Anand Singh <50056654+amritanandsingh@users.noreply.github.com>
2023-07-24 02:01:33 +0530

    done
    this repository only for learning

--- index.js
+++ index.js
@@ -1,47 +1,16 @@
 const express = require("express");
-const router = express.Router();
-const { v4: uuidv4 } = require('uuid');
-let users=[];
+const app = express();
+const port = 8000;
+const bodyParser = require('body-parser');
 
-router.get('/',(req,res)=>{
-    console.log(user);
-    
-    res.send("from router index.js it is home page");
+
+app.use(express.urlencoded());
+app.use(bodyParser.json());
+app.use("/", require("./routes/index"));
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log("server is up and running");
 });
-
-router.get('/users',(req,res)=>{
-    res.send(users);
-});
-
-router.post('/',(req,res)=>{
-    const user = req.body; 
-    users.push({...user,id:uuidv4()});
-    console.log(users);
-    res.send(`user with name ${user.firstName} is added`);
-});
-
-router.get('/:id',(req,res)=>{
-    const {id}=req.params;
-    const founduser = users.find((user)=>user.id ===id);
-    res.send(founduser);
-});
-
-router.delete('/:id',(req,res)=>{
-    const{id}=req.params;
-    users=users.filter((user)=>user.id!==id);
-    res.send("deleted given id");
-});
-
-router.patch('/:id',(req,res)=>{
-    const {id} = req.params;
-    let user = users.find((user)=> user.id===id);
-    const {firstName,lastName,age}=req.body;
-
-    if(firstName) user.firstName=firstName;
-    if(lastName) user.lastName=lastName;
-    if(age) user.age=age;
-    
-    res.send(`user with ${id} is updated`);
-
-});
-module.exports = router;
